b010eb66877c4461f540d6fdfda572f9f826ef9d
bruvv <3063928+bruvv@users.noreply.github.com>
2019-12-18 22:39:35 +0100

    Update start-instabot.sh

--- start-instabot.sh
+++ start-instabot.sh
@@ -2,6 +2,5 @@
 echo "Starting instabot for username: $IG_USER"
 echo "Session file: /app/session/$IG_USER.session"
 echo "First update"
-cd /app
-git pull
+python -m pip install instabot.py --upgrade
 python -m instabot_py -c /app/config/instabot.config.yml --login "$IG_USER" --password "$IG_PASSWORD" --sqlite-path "/app/db" --session-file "/app/session/$IG_USER.session" --ignore-updates
